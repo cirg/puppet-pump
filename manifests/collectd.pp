0271657bccf9fb59cbc8f577216d93add9981b9a
whitehatx2 <anikatesingh@gmail.com>
2013-01-28 15:36:06 -0800

    statsd and collectd

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -1,2 +1,40 @@
 class pump::collectd {
+
+file { '/opt/collectd':,
+    ensure => directory,
+    mode => '0775',
+  }
+
+exec { 'download-collectd':
+    cwd     => '/opt/collectd',
+    creates => '/opt/collectd/collectd-5.1.1.tar.gz',
+    command => '/usr/bin/wget \'http://collectd.org/files/collectd-5.1.1.tar.gz\'',
+	timeout => 5000,
+  }
+}
+
+exec { 'unzip-collectd':
+    cwd     => '/opt/collectd',
+    command => 'tar -zxvf collectd-5.1.1.tar.gz',
+	require => Exec['download-collectd'],
+	timeout => 5000,
+  }
+  
+exec { 'configure-collectd':
+    cwd     => '/opt/collectd/collectd-5.1.1',
+    command => './configure',
+	require => Exec['unzip-collectd'],
+  }
+  
+exec { 'make-collectd':
+    cwd     => '/opt/collectd/collectd-5.1.1',
+    command => './make',
+	require => Exec['configure-collectd'],
+  }
+  
+exec { 'makeinstall-collectd':
+    cwd     => '/opt/collectd/collectd-5.1.1',
+    command => './make install',
+	require => Exec['make-collectd'],
+  }  
 }
--- manifests/statsd.pp
+++ manifests/statsd.pp
@@ -1,2 +1,64 @@
 class pump::statsd {
+
+  package { [
+    'curl',
+	'build-essential',
+	'libssl-dev',
+  ]:
+    ensure => installed,
+  }
+  
+  exec{ 'nodejs-git-clone':
+    command => '/usr/bin/git clone --depth 1 git://github.com/joyent/node.git /opt/node',
+    creates => '/opt/node',
+    logoutput => 'true',
+	require => Package['libssl-dev'],
+  }
+  
+  exec{ "nodejs-install-1":
+    cwd => '/opt/node',
+    command => '/usr/bin/git checkout v0.8.9', 
+    logoutput => 'on_failure',
+	require => Exec['nodejs-git-clone'],
+  }
+  
+  exec{ "nodejs-install-2":
+    cwd => '/opt/node',
+    command => './configure --prefix=/opt/node', 
+    logoutput => 'on_failure',
+	require => Exec['nodejs-install-1'],
+  }
+  
+  exec{ "nodejs-install-3":
+    cwd => '/opt/node',
+    command => 'make', 
+    logoutput => 'on_failure',
+	require => Exec['nodejs-install-2'],
+  }
+  
+  exec{ "nodejs-install-4":
+    cwd => '/opt/node',
+    command => 'make install', 
+    logoutput => 'on_failure',
+	require => Exec['nodejs-install-3'],
+  }
+  
+  exec{ 'statsd-git-clone':
+    command => '/usr/bin/git clone https://github.com/etsy/statsd /opt/statsd',
+    creates => '/opt/statsd',
+    logoutput => 'true',
+	require => Exec['nodejs-install-4'],
+  }
+  
+  file {"/usr/share/tomcat6/.OpenMRS/openmrs-runtime.properties":
+    content => '
+  {
+ graphitePort: 2003
+, graphiteHost: "localhost"
+, port: 8125
+, backends: [ "./backends/graphite" ]
+, flushInterval: 1000
+}',
+  require => Exec['statsd-git-clone'],
+  }  
 }
