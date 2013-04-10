f4f10a185837ef1a3648c11b700374254703193f
Paul Bugni <pbugni@u.washington.edu>
2013-04-10 20:21:07 +0300

    Fine tune organizational buckets.

--- files/examples/kenyaemr_sample_query_to_statsd.sh
+++ files/examples/kenyaemr_sample_query_to_statsd.sh
@@ -1,11 +1,11 @@
 # number of HIV positive patients
-echo -n "kenyaemr.HIV_postitive_patient.count:`echo \
+echo -n "KenyaEMR_Clinical_Indicators.HIV_postitive_patient.count:`echo \
 "SELECT count(distinct person_id) FROM openmrs.obs WHERE \
 concept_id=160554;" | \
 mysql --database=openmrs --skip-column-names`|c" | nc -w 1 -u 127.0.0.1 8125
 
 # number of CD4 cell count in past 6 months 
-echo -n "kenyaemr.6mo_CD4_cell_count.count:`echo \
+echo -n "KenyaEMR_Clinical_Indicators.6mo_CD4_cell_count.count:`echo \
 "SELECT count(DISTINCT PERSON_ID) FROM openmrs.obs where concept_id = 730 \
 AND PERIOD_DIFF(EXTRACT(YEAR_MONTH FROM NOW()), EXTRACT(YEAR_MONTH \
 FROM OBS_DATETIME)) <= 6;" | \
