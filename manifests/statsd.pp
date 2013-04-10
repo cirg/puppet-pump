38a28815e2560487c39cfa24ce314bc8e3758479
Paul Bugni <pbugni@u.washington.edu>
2013-04-10 19:45:57 +0300

    Set statsd flush interval to 1 min - needs to sync with carbon settings!

--- manifests/statsd.pp
+++ manifests/statsd.pp
@@ -55,7 +55,7 @@ include pump::collectd
 , graphiteHost: "localhost"
 , port: 8125
 , backends: [ "./backends/graphite" ]
-, flushInterval: 1000
+, flushInterval: 60000
 , graphite: { legacyNamespace: false
 ,   globalPrefix: "NOT_INITIALIZED."
 ,   prefixCounter: ""
